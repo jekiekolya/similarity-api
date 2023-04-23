0e8009b93663556d874a2c2b3bcf6d39d90c2027
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-04-23 20:10:05 +0300

    base settings

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+
+// Components
+import Paragraph from '@/app/components/ui/Paragraph';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <main className="bg-red-500">
+      <Paragraph size="sm">Some text</Paragraph>
+    </main>
+  );
+}
