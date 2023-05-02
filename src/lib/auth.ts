73c6e2f7b1be59d66ba8d0a723058820fc1066bd
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-05-02 22:30:41 +0300

    connect prisma

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,17 @@
-import { NextAuthOptions } from 'next-auth';
 import { db } from '@/lib/db';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-function getGoogleCredentials() {
+function getGoogleCredentials(): { clientId: string; clientSecret: string } {
   const clientId = process.env.GOOGLE_CLIENT_ID;
   const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
   if (!clientId || clientId.length === 0) {
-    throw new Error('No clientID for google provider set');
+    throw new Error('Missing GOOGLE_CLIENT_ID');
   }
 
   if (!clientSecret || clientSecret.length === 0) {
-    throw new Error('No clientSecret for google provider set');
+    throw new Error('Missing GOOGLE_CLIENT_SECRET');
   }
 
   return { clientId, clientSecret };
@@ -35,10 +35,34 @@ export const authOptions: NextAuthOptions = {
     async session({ token, session }) {
       if (token) {
         session.user.id = token.id;
-        session.user?.name = token.name;
-        session.user?.email = token.email;
-        session.user?.image = token.image;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
       }
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+
+      if (!dbUser) {
+        token.id = user!.id;
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+    redirect() {
+      return '/dashboard';
     },
   },
 };
