00a5e5c723807136b5c5b6f9b984e50897ed228e
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-05-09 23:13:27 +0300

    Create page.tsx

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
+import { db } from '@/lib/db';
+
+export const metadata: Metadata = {
+  title: 'Similarity API | Dashboard',
+  description: 'Free & open-source text similarity API',
+};
+const page = async () => {
+  const user = await getServerSession(authOptions);
+
+  if (!user) return <p>no user</p>;
+
+  const apiKey = await db.apiKey.findFirst({
+    where: { userId: user.user.id, enabled: true },
+  });
+
+  return (
+    <div className="max-w-7xl mx-auto my-16">
+      {/* {apiKey ? <ApiDashboard /> : <RequestApiKey />} */}
+    </div>
+  );
+};
+
+export default page;
